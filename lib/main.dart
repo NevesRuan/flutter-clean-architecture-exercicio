fe9a765e790f5d846c6fc08e3940d78645035060
Ruan Da Silva Das Neves <ruan.neves@bpkedu.com.br>
2026-03-05 21:14:27 -0300

    Excercicio 1: Construcao de uma Aplicacao Flutter com Arquitetura em Camadas

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'presentation/pages/product_page.dart';
+import 'presentation/viewmodels/product_viewmodel.dart';
+import 'data/repositories/product_repository_impl.dart';
+import 'data/datasources/product_remote_datasource.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: ProductPage(
+        viewModel: ProductViewModel(
+          ProductRepositoryImpl(
+            ProductRemoteDatasource(Dio()),
+          ),
+        ),
+      ),
+    );
+  }
+}
